bed3050760ff29bd1c19c4763fef687d507f9e59
Vladimir Shatskiy <vovaysach@gmail.com>
2022-05-22 02:15:39 +0300

    refactoring: rename varibles and functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
@@ -17,22 +18,20 @@ def get_name_year(years):
         return 'лет'
 
 
-def make_wines(wine_data):
-    wines = {}
-    for wine in wine_data:
-        if wine['Категория'] not in wines:
-            wines[wine['Категория']] = []
-        wines[wine['Категория']].append(wine)
-    return wines
+def make_wine_dict_by_category(wine_list):
+    wines_by_categories = defaultdict(list)
+    for wine in wine_list:
+        wines_by_categories[wine['Категория']].append(wine)
+    return wines_by_categories
 
 
-def get_excel_data(filepath, sheet_name):
+def get_wine_list_from_excel(filepath, sheet_name):
     excel_data_df = pandas.read_excel(filepath, sheet_name=sheet_name,
                                     #   usecols=["Категория", "Название", "Сорт", "Цена", "Картинка", "Акция"],
                                       na_values='NaN', keep_default_na=False)
     excel_data_df = excel_data_df.astype({'Цена': 'int32'})
-    wine_data = excel_data_df.to_dict(orient='records')
-    return wine_data
+    wine_list = excel_data_df.to_dict(orient='records')
+    return wine_list
 
 
 def years_from_foundation():
@@ -49,14 +48,16 @@ def main():
     )
     filepath, sheet_name = return_filepath_and_sheet()
     try:
-        wine_data = get_excel_data(filepath, sheet_name)
+        wine_list = get_wine_list_from_excel(filepath, sheet_name)
     except FileNotFoundError:
         return print('Такого файла не существует!')
     except ValueError:
         return print('Такого листа в файле нет!')
 
-    wines = make_wines(wine_data)
-    wines = sorted(wines.items())
+    wines_by_categories = make_wine_dict_by_category(wine_list)
+    # wines = sorted(wines.items())
+    categories = sorted(wines_by_categories.keys())
+    print(categories)
     
     template = env.get_template('template.html')
     years = years_from_foundation()
@@ -64,7 +65,7 @@ def main():
     rendered_page = template.render(
         years=years, 
         name_year=name_year,
-        wines=wines,
+        # wines=wines,
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
@@ -76,6 +77,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # get_excel_data('wine_table.xlsx', 'df')
-    # main()
-    
